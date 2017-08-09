f7295f7765508e49197f19f31bfdb35a013cad2d
Luis de Dios Martín <luisddm@gmail.com>
2017-08-09 10:35:49 +0200

    Upload full code

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
+var browserSync = require('browser-sync');
 
 gulp.task('script', function () {
   gulp.src('src/js/*.js')
@@ -16,5 +18,20 @@ gulp.task('script', function () {
 gulp.task('style', function () {
   gulp.src('src/css/app.scss')
     .pipe(sass())
+    .pipe(cleanCss())
+    .pipe(rename('app.min.css'))
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['script', 'style']);
+
+gulp.task('watch', ['script', 'style', 'reload'], function () {
+  gulp.watch('src/css/*.scss', ['style']);
+  gulp.watch('src/js/*.js', ['script']);
+});
+
+gulp.task('reload', function () {
+  browserSync.init(['dist/css/**.css', 'dist/js/**.js', 'index.html'], {
+    server: './'
+  });
 });
